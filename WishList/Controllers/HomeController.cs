d2cf41fdecb9c8a58462726a7ca6c3073848a98b
wazolee <wazolee@gmail.com>
2022-06-11 22:19:11 +0200

    Completed module

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
         public IActionResult Error()
         {
-            return View();
+            return View("Error");
         }
     }
 }

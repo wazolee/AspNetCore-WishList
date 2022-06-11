c6932749ba6d8aa25669de1a018e308a0c021326
wazolee <wazolee@gmail.com>
2022-06-11 22:17:57 +0200

    Completed module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
